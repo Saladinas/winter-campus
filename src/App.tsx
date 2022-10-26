6023986dd46b68b715e97afd8769ebfc83aa517c
Lukas Jasmontas <lukas.jasmontas@cern.ch>
2022-10-26 19:18:55 +0200

    Integrate internationalization with i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  //Creating a method to change the language onChnage from select box
+  const changeLanguageHandler = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const languageValue = event.target.value;
+    i18n.changeLanguage(languageValue);
+  };
+
   return (
     <div className="App-winter-campus">
+      <div>
+        <select onChange={changeLanguageHandler}>
+          <option value="en">English</option>
+          <option value="pl">Polish</option>
+        </select>
+      </div>
       <Link to={'dish'}>
-        <button type="button">Dishes</button>
+        <button type="button">{t('Meals') as string}</button>
       </Link>
       <Link to={'dish/pierogi'}>
-        <button type="button">Pierogi</button>
+        <button type="button">{t('Dumplings') as string}</button>
       </Link>
       <Routes>
-        <Route path="/" element={<div>Home</div>}></Route>
-        <Route path="/dish" element={<div>Dishes</div>} />
-        <Route path="/dish/:id" element={<div>Pierogi</div>} />
+        <Route path="/" element={<div>{t('Home') as string}</div>}></Route>
+        <Route path="/dish" element={<div>{t('Meals') as string}</div>} />
+        <Route
+          path="/dish/:id"
+          element={<div>{t('Dumplings') as string}</div>}
+        />
       </Routes>
     </div>
   );
